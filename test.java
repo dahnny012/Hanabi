30db8c8fb5b615dde5f50cb8edc111043e471e7f
dahnny012 <filler012@gmail.com>
2014-12-27 21:18:09 +0000

    added wrong test

---
+++ test.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.*;
+
+public class test{
+	public static void main(String[] arg)
+	{
+		List<Integer> gg =  new ArrayList<Integer>();
+		gg.add(1);
+	}
+}
