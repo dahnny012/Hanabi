3f157ae358b241f39872d9014fcf35dfac916b94
dahnny012 <filler012@gmail.com>
2014-12-28 00:04:21 +0000

    psuedocode game manager

---
+++ GameManager.java
@@ -0,0 +1,30 @@
+public class GameManager{
+	
+	private int score = 0;
+	private fireworksTokens = 0;
+	private timeTokens = 8;
+	public static void main(String[] args){
+		// Make a deck
+		// instantiate the players
+		// go through each player's turn.	
+	}
+	public boolean endGame()
+	{
+		// Check for 0 cards in deck.
+		// Check for 3 fireworks tokens
+		// Check for 5 on top each pile.
+	}
+	public int getScore()
+	{
+		return score;
+	}
+	public int getFireworksTokens()
+	{
+		return fireworksTokens;	
+	}
+	public int getTimeTokens()
+	{
+		return timeTokens;
+	}
+	
+}
