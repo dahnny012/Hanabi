41d27d594582492ffdba461128a8cde1a0370fe9
sudowoodo <giraffapotamus@gmail.com>
2015-01-01 17:21:16 -0600

    Handsize is dependent on Player Count
    For 2-3 players, the hand size is 5.
    For 4-5 players, the hand size is 4.

--- Board/Board.java
+++ Board/Board.java
@@ -1,37 +1,38 @@
 public class Board{
-    public Deck deck;
-    public ArrayList<Player> Players;
-    public int numPlayers;
-    public int fireworksTokens = 3;
-    public int timeTokens = = 8;
-    public boolean gameInProgress = false;
-    // maxScore tracks the maximum possible total with the remaining cards
+	public Deck deck;
+	public ArrayList<Player> Players;
+	public int numPlayers;
+	public int fireworksTokens = 3;
+	public int timeTokens = = 8;
+	public boolean gameInProgress = false;
+	// maxScore tracks the maximum possible total with the remaining cards
 	// it does NOT track the maximum possible total with the remaining turns
 	// (eg. if two players, each with hands of five 1s, draw the last card
 	// 	without anything on the board, maxScore = 5, not 2)
 	// We could do it, but I think it's unnecessary work.
 	
-    public int score = 0;
-    public int maxScore =25;
-    public boardIndex;
-    // currentTurn is an int within [0, n), where n = playerCount
+	public int score = 0;
+	public int maxScore = 25;
+	public boardIndex;
+	// currPlayer is an int within [0, n), where n = playerCount
 	private int currPlayer = 0;
 	private int gmAddr;
 	private Socket gameManager;
+	
 	public Board(int num,int boardIndex,int gmAddr)
-    {
-        this.boardIndex = boardIndex;
-        deck = new Deck();
-        stacksMap = new HashMap();
-        this.gmAddr = gmAddr;
-        //open a socket to gameManager and keep it.
-    }
+	{
+		this.boardIndex = boardIndex;
+		deck = new Deck();
+		stacksMap = new HashMap();
+		this.gmAddr = gmAddr;
+		//open a socket to gameManager and keep it.
+	}
 	
 	public Move playOneTurn()
 	{
-	    askForMove(Players.get(currPlayer));
-	    // Do neccessary actions
-	    // alert game manager to change state of other players.
+		askForMove(Players.get(currPlayer));
+		// Do neccessary actions
+		// alert game manager to change state of other players.
 	}
 	
 	public Move askForMove(Player current)
@@ -39,41 +40,45 @@ public class Board{
 	   // Get move from player
 	   return null;
 	}
-    
-    public void addPlayer()
-    {
-        // probably add their network connection stuff later.
-        if(gameInProgress != progress)
-         Players.add(new Player());
-    }
-    
-    public void startGame()
-    {
-        gameInProgress = true;
-        dealCards();
-        playOneTurn();
-    }
-    
-    public dealCards()
-    {
-        playerCount = Players.size();
-        for (i = 0; i < playerCount; i++){
-			for (j = 0; j < 5; j++){
+
+	public void addPlayer()
+	{
+		// probably add their network connection stuff later.
+		if(gameInProgress != progress)
+			Players.add(new Player());
+	}
+
+	public void startGame()
+	{
+		gameInProgress = true;
+		dealCards();
+		playOneTurn();
+	}
+
+	public dealCards()
+	{
+		playerCount = Players.size();
+		int handSize;
+		if (playerCount < 2 || playerCount > 5){
+			//error out
+		}else if (playerCount <= 3){
+			handSize = 5;
+		}else{
+			handSize = 4;
+		}
+		for (i = 0; i < playerCount; i++){
+			Players.get(i).maxHandSize = handSize;
+			for (j = 0; j < handSize; j++){
 				 Players.get(i).hand[i] = deck.draw();
 			}
 		}
-    }
-    
-    public boolean endGame()
-    {
-        if(fireworksTokens >= 3 || countdown == 0 || score == maxScore)
-            return true;
-        return false
-    }
-    
-    
-    
-    
-    
-    
+	}
+
+	public boolean endGame()
+	{
+		if(fireworksTokens >= 3 || countdown == 0 || score == maxScore)
+			return true;
+		return false;
+	}
+
 }
