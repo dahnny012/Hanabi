57c1bddf61290e39440911ba327f742cf92de290
dahnny012 <nguy1952@umn.edu>
2015-02-07 02:36:29 -0600

    deleting files

--- GameManager/GameManager.java
+++
@@ -1,9 +0,0 @@
-public class GameManager{
-    // Has list of board sockets
-    public void checkBoards()
-    {
-        // Socket read each board socket.
-            // Yes there is a move.
-            // Write it to all players on the board.
-    }
-}
