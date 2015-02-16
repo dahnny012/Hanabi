cb401d0329f603395c48be09d2806934bd9b6660
dahnny012 <filler012@gmail.com>
2015-02-15 23:32:38 -0600

    Took a break from hw, prototyped threadpool

--- Server.java
+++ Server.java
@@ -5,18 +5,13 @@ import java.net.ServerSocket;
 import java.io.*;
 
 
-/*Todo
-* Setup for multiple threads well need this for each connected player. 
-* Write back to players.
-*/
 
 class Handlers{
-	HashMap<Integer,List<Socket>> rooms = new HashMap<Integer,List<Socket>>();
-	
-	public void join(Socket client,BufferedReader req,PrintWriter res){
+	HashMap<Integer,List<ClientSocket>> rooms = new HashMap<Integer,List<ClientSocket>>();
+	public void join(ClientSocket client,BufferedReader req,PrintWriter res){
 		try{
-		int roomNum = Integer.parseInt(req.readLine());
-		List<Socket> room = rooms.get(roomNum);
+			int roomNum = Integer.parseInt(req.readLine());
+			List<ClientSocket> room = rooms.get(roomNum);
 			room.add(client);
 			System.out.println("Added player to room " + roomNum);
 			res.println("JOINED " + roomNum);
@@ -29,8 +24,8 @@ class Handlers{
 		}
 	}
 	
-	public void create(Socket client,BufferedReader req,PrintWriter res){
-		List<Socket> sockets = new ArrayList<Socket>();
+	public void create(ClientSocket client,BufferedReader req,PrintWriter res){
+		List<ClientSocket> sockets = new ArrayList<ClientSocket>();
 		Random rando = new Random();
 		int roomNumber = rando.nextInt(10000);
 		
@@ -45,18 +40,32 @@ class Handlers{
 
 
 class Router{
-	Handlers handle = new Handlers();
-	public void handle(Socket client) throws IOException{
+	Handlers handle;
+	private static Router router;
+	private Router(){
+		handle = new Handlers();
+	}
+	
+	public static Router getInstance(){
+		if(router == null)
+            router = new Router();
+        return router;
+	}
+	
+	public void handle(ClientSocket client) throws IOException{
 		String route;
-		PrintWriter res = new PrintWriter(client.getOutputStream());
-		BufferedReader req = new BufferedReader(new InputStreamReader(client.getInputStream()));
-		res.println("Hello , welcome");
-		res.flush();
+		BufferedReader req = new BufferedReader(new InputStreamReader(client.socket.getInputStream()));
+		PrintWriter res = new PrintWriter(client.socket.getOutputStream());
 		try{
-			route = req.readLine();
-			System.out.println("Route: " + route);
+			if(req.ready()){
+				route = req.readLine();
+				System.out.println("Route: " + route);
+			}else{
+				return;
+			}
 		}
 		catch(IOException io){
+			System.out.println(io);
 			return;
 		}
 		switch(route){
@@ -76,18 +85,21 @@ class Router{
 
 public class Server{
 	ServerSocket server;
-	Router router;
 	public Server() throws IOException{
 		server = new ServerSocket(8000);
-		router = new Router();
 	}
 	
 	public void start() throws IOException{
-		while(true){
-			Socket client = server.accept();
-			System.out.println("A client has connected");
-			router.handle(client);
-		}	
+		Dispatch[] dispatchers = new Dispatch[3];
+		Worker[] workers = new Worker[5];
+		for(int i=0; i<3; i++){
+			dispatchers[i] = (new Dispatch(server));
+			dispatchers[i].run();
+		}
+		for(int i=0; i<5; i++){
+			workers[i] = (new Worker());
+			workers[i].run();
+		}
 	}
 	
 	public void log(String arg)
@@ -95,3 +107,84 @@ public class Server{
 		System.out.println(arg);
 	}
 }
+
+
+class Dispatch implements Runnable{
+	ServerSocket server;
+	Clients clients = Clients.getInstance();
+	public Dispatch(ServerSocket server){
+		this.server = server;
+	}
+	
+	@Override	
+	public void run() {
+		while(true){
+			try {
+				Socket socket = server.accept();
+				ClientSocket client = new ClientSocket(socket);
+				clients.addConnection(client);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
+
+class Worker implements Runnable{
+	Clients clients = Clients.getInstance();
+	@Override
+	public void run() {
+		while(true){
+			ClientSocket task = clients.getWork();
+			if(task != null){
+				Router route = Router.getInstance();
+				try {
+					route.handle(task);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				clients.addConnection(task);
+			}
+		}
+		
+	}	
+}
+
+class ClientSocket{
+	Socket socket;
+	public int id;
+	public int roomNumber;
+	public ClientSocket(Socket socket){
+		Random rando = new Random();
+		id = rando.nextInt(10000);
+		this.socket = socket;
+	}
+}
+
+
+class Clients{
+	private static Clients clients;
+	private ArrayList<ClientSocket> connections;
+	private Clients(){
+		this.connections = new ArrayList<ClientSocket>();
+	}
+	
+	public static Clients getInstance(){
+		if(clients == null)
+            clients = new Clients();
+        return clients;
+	}
+	
+	public void addConnection(ClientSocket client){
+		connections.add(client);
+	}
+	
+	public ClientSocket getWork(){
+		if(connections.isEmpty())
+			return null;
+		return connections.remove(0);
+	}
+}
+
+
+
--- TODOS.md
+++ TODOS.md
@@ -1,9 +1,8 @@
 Hanabi TODOS:
 
 1. Server needs 3 functions
-  - Recieves a ping to join or create a room
-  - Add sockets to room
-  	- Need to make threads to handle routing logic
+  - Recieves a ping to join or create a room -- check
+  - Add sockets to room -- check
   	- GameRoom should be some sort of object
   	- Need to be able to broadcast in a room.
   	
